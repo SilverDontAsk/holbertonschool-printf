d1020b8a82b83183e2bc39f8f8eb0398dc7c70e4
abdieljrg <abdieljrg@gmail.com>
2024-03-30 00:16:45 -0400

    x format

---
+++ xformat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * xformat - %x format for printf
+ * @arg: arguments
+ * @c:variable of number of chars printed
+ * Return: Void
+ */
+void xformat(va_list arg, int *c)
+{
+unsigned int num = va_arg(arg, unsigned int);
+char buffer[12];
+int len = 0;
+
+	if (num == 0)
+	{
+		putchar('0');
+		(*c)++;
+		return;
+	}
+	while (num > 0)
+	{
+		int rem = num % 16;
+
+		buffer[len++] = (rem < 10) ? rem + '0' : rem - 10 + 'a';
+		num /= 16;
+	}
+	for (int i = 0, j = len - 1; i < j; i++, j--)
+	{
+		char temp = buffer[i];
+
+		buffer[i] = buffer[j];
+		buffer[j] = temp;
+	}
+	for (int i = 0; i < len; i++)
+	{
+		putchar(buffer[i]);
+		(*c)++;
+	}
+}
