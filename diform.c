01fa1b9e5ff63e9bd73015b89441d0358a8946b9
SilverDontAsk <steven_rosario@penguin>
2024-03-30 22:52:53 -0400

    int min fix

--- diform.c
+++ diform.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <limits.h>
 /**
  * dformat - handles the integers in printf
  * @arg: list of inputs
@@ -12,7 +13,14 @@ void diformat(va_list arg, int *count)
 {
 	int num = va_arg(arg, int);
 	int divisor = 1;
-	if (num < 0)
+	if (num == INT_MIN)
+	{
+		putchar('-');
+		putchar('2');
+		num = 147483648;
+		*count += 2;
+	}
+	else if (num < 0)
 	{
 		putchar('-');
 		num = -num;
